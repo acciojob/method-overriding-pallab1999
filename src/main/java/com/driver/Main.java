4b04a9fc2b6a34136fc80136483a89894ffcc0e4
pallab1999 <pallab.dutta1999@gmail.com>
2025-01-17 12:37:29 +0530

    solved Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -18,11 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
          B b = new B();
-
-        new A().meth();
-        b.meth();
+        System.out.println(new A().meth());
+        System.out.println(b.meth());
     }
-
-
-  
 }
